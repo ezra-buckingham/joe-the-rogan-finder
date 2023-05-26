e1339bbac574ea63c88445142031f3a4bbbb2fe8
ezra-buckingham <ezra.buckingham@gmail.com>
2023-05-26 16:58:38 -0400

    Added basic code

---
+++ README.md
@@ -0,0 +1,5 @@
+## Usage
+
+```bash
+python3 -m pip install -r requirements.txt
+```
---
+++ main.py
@@ -0,0 +1,48 @@
+import requests
+import click
+from bs4 import BeautifulSoup
+from pathlib import Path
+import json
+
+def send_notification(webhook_url, message):
+    
+    data = {
+        'content': message
+    }
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post(webhook_url, data=json.dumps(data), headers=headers)
+    return response
+
+
+@click.command()
+@click.option('-d', '--date', required=True, help='''
+    Date to check for on the website (ex. October 23, 2023)
+''')
+@click.option('-u', '--url', required=True, help='''
+    Discord URL to notify when found
+''')
+def cli(date, url):
+    
+    # Create the path for the file to create if there was already a found item
+    found_file = Path('/tmp/joe-found')
+    if found_file.exists(): exit()
+    
+    # Send a GET request to the website
+    url = 'https://comedymothership.com/shows'
+    response = requests.get(url)
+    html = response.text
+
+    # Parse the HTML and extract the date
+    soup = BeautifulSoup(html, 'html.parser')
+    date_elements = soup.findAll('div', class_='h6')
+    
+    for current_date in date_elements:
+        current_date = current_date.text.strip()
+        if not current_date == date: continue
+        message = f'The date "{date}" was found on Joe Rogan\'s website!'
+        send_notification(url, message)
+        found_file.touch()
+
+
+if __name__ == "__main__":
+    cli()
